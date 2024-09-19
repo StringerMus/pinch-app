6c46a0ccc7bc0370b72e49d3ec9dd9e8c4b4362f
Musaddek Ali <mussali.one@gmail.com>
2024-09-19 10:24:25 +0000

    Create navbar and add icons.

---
+++ src/components/NavBar.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Navbar, Container, Nav } from "react-bootstrap";
+// Crreate and Add a logo - import logo from '../assets/logo.png'
+
+const NavBar = () => {
+  return (
+    <Navbar expand="md" fixed="top">
+        <Container>
+            <Navbar.Brand>
+                <h2>Pinch</h2>
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="ml-auto text-right">
+                    <Nav.Link>
+                        <i className="fa-solid fa-house-chimney"></i>Home</Nav.Link>
+                    <Nav.Link>
+                    <i className="fa-solid fa-right-to-bracket"></i>Sign-in</Nav.Link>
+                    <Nav.Link>
+                    <i className="fa-solid fa-user-plus"></i>Sign-up</Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+        </Container>
+    </Navbar>
+  );
+};
+
+export default NavBar
