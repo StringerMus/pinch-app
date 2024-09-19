e458fd380e85ea45f2629d1f2ea9090cb04e524e
Musaddek Ali <mussali.one@gmail.com>
2024-09-19 13:16:15 +0000

    Create routes for navlinks and add pagelinks. Add to NavBar css.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,29 +1,53 @@
-import React from 'react'
+import React from 'react';
 import { Navbar, Container, Nav } from "react-bootstrap";
-// Crreate and Add a logo - import logo from '../assets/logo.png'
-import styles from "../styles/NavBar.module.css"
+import styles from "../styles/NavBar.module.css";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <Navbar className={styles.NavBar} expand="md" fixed="top">
-        <Container>
-            <Navbar.Brand>
-                <h2>Pinch</h2>
-            </Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="ml-auto text-right">
-                    <Nav.Link>
-                        <i className="fa-solid fa-house-chimney"></i>Home</Nav.Link>
-                    <Nav.Link>
-                    <i className="fa-solid fa-right-to-bracket"></i>Sign-in</Nav.Link>
-                    <Nav.Link>
-                    <i className="fa-solid fa-user-plus"></i>Sign-up</Nav.Link>
-                </Nav>
-            </Navbar.Collapse>
-        </Container>
+      <Container>
+        {/* Use NavLink for the brand logo */}
+        <NavLink to="/">
+          <Navbar.Brand>
+            <h2>Pinch</h2>
+          </Navbar.Brand>
+        </NavLink>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto text-right">
+            {/* NavLink for Home */}
+            <NavLink
+                exact
+                to="/"
+                className="NavLink"
+                activeClassName={styles.Active}
+            >
+              <i className="fa-solid fa-house-chimney"></i> Home
+            </NavLink>
+
+            {/* NavLink for Sign-in */}
+            <NavLink
+                to="/signin"
+                className="NavLink"
+                activeClassName={styles.Active}
+            >
+              <i className="fa-solid fa-right-to-bracket"></i> Sign-in
+            </NavLink>
+
+            {/* NavLink for Sign-up */}
+            <NavLink
+                to="/signup"
+                className="NavLink"
+                activeClassName={styles.Active}
+            >
+              <i className="fa-solid fa-user-plus"></i> Sign-up
+            </NavLink>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
     </Navbar>
   );
 };
 
-export default NavBar
+export default NavBar;
