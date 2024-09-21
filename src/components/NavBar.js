ee2d020a5e070afec8258e654dae175352a4bbba
Musaddek Ali <mussali.one@gmail.com>
2024-09-21 14:22:03 +0000

    Add navbar options for signed in users and create logout function.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,17 +2,59 @@ import React from 'react';
 import { Navbar, Container, Nav } from "react-bootstrap";
 import styles from "../styles/NavBar.module.css";
 import { NavLink } from "react-router-dom";
-import { useCurrentUser } from '../contexts/CurrentUserContext';
+import { useCurrentUser, useSetCurrentUser } from '../contexts/CurrentUserContext';
+import axios from 'axios';
 
 const NavBar = () => {
   const currentUser = useCurrentUser();
+  const setCurrentUser = useSetCurrentUser();
 
-  const loggedInIcons = <>{currentUser?.username}</>;
+  const handleSignOut = async () => {
+    try {
+      await axios.post('dj-rest-auth/logout/');
+      setCurrentUser(null);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const addPostIcon = (
+    <NavLink
+      to="/listings/create"
+      className="NavLink"
+      activeClassName={styles.Active}
+    >
+      <i className="fa-regular fa-square-plus"></i> List item
+    </NavLink>
+  );
+  const loggedInIcons = (
+  <>
+    <NavLink
+      to="/liked"
+      className={styles.NavLink}
+      activeClassName={styles.Active}
+    >
+      <i className="fa-solid fa-heart"></i>Liked
+    </NavLink>
+    <NavLink
+      to="/" onClick={handleSignOut}
+      className={styles.NavLink}
+    >
+      <i className="fa-solid fa-right-from-bracket"></i>Sign out
+    </NavLink>
+    <NavLink
+      className={styles.NavLink}
+      to={`/profiles/${currentUser?.profile_id}`}
+    >
+      <Avatar src={currentUser?.profile_image} text="Profile" height={40} />
+    </NavLink>
+  </>
+  );
   const loggedOutIcons = (
     <>
       <NavLink
         to="/signin"
-        className="NavLink"
+        className={styles.NavLink}
         activeClassName={styles.Active}
       >
         <i className="fa-solid fa-right-to-bracket"></i> Sign-in
@@ -20,7 +62,7 @@ const NavBar = () => {
 
       <NavLink
         to="/signup"
-        className="NavLink"
+        className={styles.NavLink}
         activeClassName={styles.Active}
       >
         <i className="fa-solid fa-user-plus"></i> Sign-up
@@ -37,6 +79,9 @@ const NavBar = () => {
             <h2>Pinch</h2>
           </Navbar.Brand>
         </NavLink>
+        {/* create a lisitng */}
+        {currentUser && addPostIcon}
+
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-right">
