7976b3a7fd0f2a121f3d3e3d939a01e9f9674e1c
Musaddek Ali <mussali.one@gmail.com>
2024-09-21 13:33:24 +0000

    Creatie axios Res and Req interceptors to refresh access tokens.

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -3,3 +3,6 @@ import axios from "axios";
 axios.defaults.baseURL = 'https://pinch-api-f947cf5f7bdc.herokuapp.com/'
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
 axios.defaults.withCredentials = true;
+
+export const axiosReq = axios.create(); /*intercepts login request*/
+export const axiosRes = axios.create(); /*intercepts login response*/
