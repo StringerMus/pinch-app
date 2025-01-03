2d786438d7a583b1a9cc57fc6cb3dbc90e1b5394
Musaddek Ali <mussali.one@gmail.com>
2025-01-03 21:33:54 +0000

    Amend Query form layout.

--- src/pages/posts/Email.js
+++ src/pages/posts/Email.js
@@ -10,31 +10,28 @@ import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
 import Alert from "react-bootstrap/Alert";
 
-// Modal styles
-// const modalStyles = {
-//   content: {
-//     width: "700px",
-//     margin: "auto",
-//     height: "auto",
-//     padding: "20px",
-//     borderRadius: "20px",
-//   },
-// };
+import styles from "../../styles/PostCreateEditForm.module.css";
+import appStyles from "../../App.module.css";
+import btnStyles from "../../styles/Button.module.css";
+
+import { useNotification } from "../../contexts/NotificationContext";
 
 Modal.setAppElement("#root"); // To avoid accessibility warnings
 
-const Email = ({ listingId, ownerEmail, itemName }) => {
+const Email = ({ listingId, ownerEmail, itemName, location, contact_email }) => {
   const [modalIsOpen, setModalIsOpen] = useState(false); /* A boolean that toggles between true and false to open and close the modal. */
   const currentUser = useCurrentUser(); // Access current user's data
 
   const [formData, setFormData] = useState({
+    item: `${itemName}`,
+    location: `${location}`,
     name: currentUser?.username || "", // Autofill with username
-    email: currentUser?.email || "",
-    subject: itemName
-      ? `Enquiry about "${itemName}"`
-      : "Enquiry about your listing", // Placeholder subject
-    message: "Hi, I am interested in your listing. Is this item available and what is the cost?", // Placeholder message
+    email: "",
+    subject: `Pinch - Enquiry about "${itemName}"`,
+    message: "Hi, I am interested in your listing. Is this item available?", // Placeholder message
   });
+
+  const showNotification = useNotification();
 
   const [loading, setLoading] = useState(false);
 
@@ -78,101 +75,112 @@ const Email = ({ listingId, ownerEmail, itemName }) => {
   };
 
   return (
-    <div>
+    <Container>
       <Button
         onClick={openModal}
         className="btn btn-primary"
         size="sm"
-        style={{ marginTop: "10px", marginBottom: "15px" }}
+        style={{ marginTop: "10px", marginBottom: "20px", marginLeft: "-15px" }}
       >
         Contact Owner
       </Button>
 
-      <Modal isOpen={modalIsOpen} onRequestClose={closeModal} contentLabel="Send Email Modal">
-        <Container>
-          <h2 className="text-center mb-4">Send a Query</h2>
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        contentLabel="Send Email Modal"
+      >
+        <Row className="justify-content-center">
+          <Col xs={12} sm={10} md={8} lg={6}>
+            <Container
+              className="p-md-5"
+            >
+              <Form onSubmit={handleSubmit}>
+                <h2 className={`text-center mb-4 ${styles.Title}`}>Send a Query</h2>
+              
+                <Form.Group as={Row} className="mb-3">
+                  <Form.Label>
+                    Name:
+                  </Form.Label>
+                    <Form.Control
+                      type="text"
+                      name="name"
+                      placeholder="Enter your name"
+                      value={formData.name}
+                      onChange={handleChange}
+                      required
+                    />
+                </Form.Group>
 
-          <Form onSubmit={handleSubmit}>
-            <Form.Group as={Row} className="mb-3">
-              <Form.Label>
-                Name:
-              </Form.Label>
-                <Form.Control
-                  type="text"
-                  name="name"
-                  placeholder="Enter your name"
-                  value={formData.name}
-                  onChange={handleChange}
-                  required
-                />
-            </Form.Group>
+                <Form.Group as={Row} className="mb-3">
+                  <Form.Label>
+                    Email:
+                  </Form.Label>
+                    <Form.Control
+                      type="email"
+                      name="email"
+                      placeholder="Enter your email"
+                      value={formData.email}
+                      onChange={handleChange}
+                      required
+                    />
+                </Form.Group>
 
-            <Form.Group as={Row} className="mb-3">
-              <Form.Label>
-                Email:
-              </Form.Label>
-                <Form.Control
-                  type="email"
-                  name="email"
-                  placeholder="Enter your email"
-                  value={formData.email}
-                  onChange={handleChange}
-                  required
-                />
-            </Form.Group>
+                <Form.Group as={Row} className="mb-3">
+                  <Form.Label>
+                    Subject:
+                  </Form.Label>
+                    <Form.Control
+                      type="text"
+                      name="subject"
+                      value={formData.subject}
+                      onChange={handleChange}
+                      placeholder="Enter a subject for your query"
+                      required
+                    />
+                </Form.Group>
 
-            <Form.Group as={Row} className="mb-3">
-              <Form.Label>
-                Subject:
-              </Form.Label>
-                <Form.Control
-                  type="text"
-                  name="subject"
-                  value={formData.subject}
-                  onChange={handleChange}
-                  placeholder="Enter a subject for your query"
-                  required
-                />
-            </Form.Group>
+                <Form.Group as={Row} className="mb-3">
+                  <Form.Label>
+                    Message:
+                  </Form.Label>
+                    <Form.Control
+                      as="textarea"
+                      name="message"
+                      value={formData.message}
+                      onChange={handleChange}
+                      rows={3}
+                      placeholder="Write your message here..."
+                      required
+                    />
+                </Form.Group>
 
-            <Form.Group as={Row} className="mb-3">
-              <Form.Label>
-                Message:
-              </Form.Label>
-                <Form.Control
-                  as="textarea"
-                  name="message"
-                  value={formData.message}
-                  onChange={handleChange}
-                  rows={5}
-                  placeholder="Write your message here..."
-                  required
-                />
-            </Form.Group>
+                <Row className="mt-4">
+                  <Col className="text-center">
+                    <Button
+                      type="submit"
+                      className="btn btn-primary me-2"
+                      disabled={loading}
+                      margin
+                    >
+                      {loading ? <span className="spinner"></span> : "Send Email"}
+                    </Button>
+                    <Button
+                      type="button"
+                      className="btn btn-secondary m-2 mb-2"
+                      onClick={closeModal}
+                    >
+                      Cancel
+                    </Button>
+                  </Col>
+                </Row>
+              </Form>
+            </Container>
+          </Col>
+        </Row>
 
-            <Row className="mt-4">
-              <Col className="text-center">
-                <Button
-                  type="submit"
-                  className="btn btn-primary me-2"
-                  disabled={loading}
-                  margin
-                >
-                  {loading ? <span className="spinner"></span> : "Send Email"}
-                </Button>
-                <Button
-                  type="button"
-                  className="btn btn-secondary m-2 mb-2"
-                  onClick={closeModal}
-                >
-                  Cancel
-                </Button>
-              </Col>
-            </Row>
-          </Form>
-        </Container>
       </Modal>
-    </div>
+    </Container>
   );
 };
 
