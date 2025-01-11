f0b332ad2b5a9baa93df0918c19ed09679b924e6
Musaddek Ali <mussali.one@gmail.com>
2025-01-11 15:13:47 +0000

    Amend axiosDefault back to previous code to fix CORS bug.

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -4,17 +4,5 @@ axios.defaults.baseURL = 'https://pinch-api-f947cf5f7bdc.herokuapp.com/';
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data';
 axios.defaults.withCredentials = true;
 
-// Create separate axios instances for requests and responses
-export const axiosReq = axios.create(); 
+export const axiosReq = axios.create();
 export const axiosRes = axios.create();
-
-// Add a function to handle login
-export const loginUser = async (signInData) => {
-    try {
-      const { data } = await axios.post("/dj-rest-auth/login/", signInData);
-      return data; // Return the login response data (e.g., user info or tokens)
-    } catch (error) {
-      throw error.response?.data || error; // Pass the error to the caller
-    }
-  };
-  
